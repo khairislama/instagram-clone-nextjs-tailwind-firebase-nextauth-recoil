f9f957e0453551710c9bc79d20b95d9441329427
khairislama <khairi.slama.1996.br@gmail.com>
2022-04-28 14:28:39 +0200

    br

--- components/Post.tsx
+++ components/Post.tsx
@@ -125,7 +125,10 @@ function Post({ id, username, userImg, img, caption }: Props) {
       {/* Caption */}
       <p className="truncate p-5">
         {likes.length > 0 && (
-          <span className="mb-1 font-bold">{likes.length} likes</span>
+          <span className="mb-1 font-bold">
+            {likes.length} likes
+            <br />
+          </span>
         )}
         <span className="mr-1 font-bold">{username}: </span>
         {caption}
